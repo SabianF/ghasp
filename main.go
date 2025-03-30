548cf3ec856f18e270a59f6360401077d1f30f73
SabianF <58588133+SabianF@users.noreply.github.com>
2025-03-30 15:33:55 -0700

    feat: create server

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+)
+
+//? GHASP Stack
+//? - Go
+//? - HTMX (Front end) + Templ
+//? - AlpineJS (Front-end interactivity)
+//? - Shoelace (UI component library) (TailwindCSS)
+//? - PostgreSQL (Database)
 
 func main() {
-	fmt.Println("Hello, world!")
+	handleSigTerm()
+	router := initRouter()
+	listenAndServe(router)
+}
+
+// Allows graceful shutdown
+func handleSigTerm() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Received SIGTERM. Exiting...")
+		os.Exit(1)
+	}()
+}
+
+func initRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", handleRoot)
+	http.Handle("/", router)
+	return router
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
+
+func listenAndServe(router *mux.Router) {
+	const port string = ":3333"
+	fmt.Printf("Starting server on port %s ...\n", port)
+
+	err := http.ListenAndServe(port, router)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed\n")
+
+	} else if err != nil {
+		fmt.Printf("Error with server: %s\n", err)
+		os.Exit(1)
+	}
 }
