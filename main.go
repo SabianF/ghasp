8b03cdd66558f1f7ff44ec43f8a76bddc338e8b3
SabianF <58588133+SabianF@users.noreply.github.com>
2025-04-16 00:29:47 -0700

    refactor: impl CA mv db to sources

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"flag"
 	"fmt"
@@ -13,12 +12,11 @@ import (
 	"syscall"
 	"time"
 
+	db_postgres "github.com/SabianF/ghasp/src/common/data/sources"
 	"github.com/SabianF/ghasp/src/common/presentation/components"
-	"github.com/SabianF/ghasp/src/common/data/models"
 	"github.com/SabianF/ghasp/src/common/presentation/pages"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -46,7 +44,7 @@ var tablePropsFooters = []string{
 func main() {
 	handleSigTerm()
 	loadEnvironmentVariables()
-	initDb()
+	db_postgres.InitDb()
 	router := initRouter()
 	listenAndServe(router)
 }
@@ -56,56 +54,6 @@ func loadEnvironmentVariables() {
 	if (err != nil) {
 		log.Fatal("Error loading .env")
 	}
-}
-
-func initDb() {
-	log.Println("Opening DB connection...")
-
-	host := os.Getenv("DB_HOST")
-	if (host == "") {
-		log.Fatal("Failed to get DB_HOST from .env")
-	}
-
-	port := os.Getenv("DB_PORT")
-	if (port == "") {
-		log.Fatal("Failed to get DB_PORT from .env")
-	}
-
-	user := os.Getenv("DB_USER")
-	if (host == "") {
-		log.Fatal("Failed to get DB_USER from .env")
-	}
-
-	pass := os.Getenv("DB_PASS")
-	if (host == "") {
-		log.Fatal("Failed to get DB_PASS from .env")
-	}
-
-	name := os.Getenv("DB_NAME")
-	if (host == "") {
-		log.Fatal("Failed to get DB_NAME from .env")
-	}
-
-	dataSource := fmt.Sprintf(
-		// The '' around password is to include any spaces
-		"host=%s port=%s user=%s password='%s' dbname=%s sslmode=disable",
-    host, port, user, pass, name,
-	)
-
-	db, err := pgx.Connect(context.Background(), dataSource)
-	if (err != nil) {
-		log.Fatal("Failed to open DB: ", err)
-	}
-	defer db.Close(context.Background())
-
-	err = db.Ping(context.Background())
-	if (err != nil) {
-		log.Fatal("Failed to establish connection to DB: ", err)
-	}
-
-	log.Printf("Successfully opened DB connection: %s\n", db.Config().Database)
-
-	models.CreateUserTable(db)
 }
 
 // Allows graceful shutdown
---
+++ src/common/data/sources/db_postgres.go
@@ -0,0 +1,62 @@
+package db_postgres
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/SabianF/ghasp/src/common/data/models"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func InitDb() {
+	log.Println("Opening DB connection...")
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		log.Fatal("Failed to get DB_HOST from .env")
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		log.Fatal("Failed to get DB_PORT from .env")
+	}
+
+	user := os.Getenv("DB_USER")
+	if host == "" {
+		log.Fatal("Failed to get DB_USER from .env")
+	}
+
+	pass := os.Getenv("DB_PASS")
+	if host == "" {
+		log.Fatal("Failed to get DB_PASS from .env")
+	}
+
+	name := os.Getenv("DB_NAME")
+	if host == "" {
+		log.Fatal("Failed to get DB_NAME from .env")
+	}
+
+	dataSource := fmt.Sprintf(
+		// The '' around password is to include any spaces
+		"host=%s port=%s user=%s password='%s' dbname=%s sslmode=disable",
+		host, port, user, pass, name,
+	)
+
+	db, err := pgx.Connect(context.Background(), dataSource)
+	if err != nil {
+		log.Fatal("Failed to open DB: ", err)
+	}
+	defer db.Close(context.Background())
+
+	err = db.Ping(context.Background())
+	if err != nil {
+		log.Fatal("Failed to establish connection to DB: ", err)
+	}
+
+	log.Printf("Successfully opened DB connection: %s\n", db.Config().Database)
+
+	models.CreateUserTable(db)
+}
