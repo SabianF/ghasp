192bf03766d22cc685d7938db0247def26118b7d
SabianF <58588133+SabianF@users.noreply.github.com>
2025-04-18 23:45:31 -0700

    feat: remove all pages except main
    BREAKING CHANGE: overhauling project to implement features named in the
    stack (GHASP) to finalize as example project

--- main.go
+++ main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/SabianF/ghasp/src/common/data/models"
 	db_postgres "github.com/SabianF/ghasp/src/common/data/sources"
-	"github.com/SabianF/ghasp/src/common/presentation/components"
 	"github.com/SabianF/ghasp/src/common/presentation/pages"
 
 	"github.com/gorilla/mux"
@@ -27,20 +24,6 @@ import (
 //? - AlpineJS (Front-end interactivity)
 //? - Shoelace (UI component library) (TailwindCSS)
 //? - PostgreSQL (Database)
-
-
-
-var tablePropsHeadings = []string{
-	"1 heading 1",
-	"2 heading 2",
-	"3 heading 3",
-}
-
-var tablePropsFooters = []string{
-	"1 footer 1",
-	"2 footer 2",
-	"3 footer 3",
-}
 
 func main() {
 	handleSigTerm()
@@ -74,23 +57,7 @@ func handleSigTerm() {
 func initRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", handleRoot)
-	router.HandleFunc("/table", handleTable)
-	router.HandleFunc("/db", handleDb)
-	router.HandleFunc("/multi-replace", handleMultiReplace)
-	router.HandleFunc("/multi-replace-button", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("HX-Trigger", "one, two, three")
-	})
-	router.HandleFunc("/test-one", func(w http.ResponseWriter, r *http.Request) {
-		pages.TestComponent("one: " + time.Now().String()).Render(r.Context(), w)
-	})
-	router.HandleFunc("/test-two", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(2 * time.Second)
-		pages.TestComponent("two: " + time.Now().String()).Render(r.Context(), w)
-	})
-	router.HandleFunc("/test-three", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-		pages.TestComponent("three: " + time.Now().String()).Render(r.Context(), w)
-	})
+	router.HandleFunc("/htmx", handleHtmx)
 	serveStaticFiles(router)
 	router.Use(middlewareLogRequests)
 	http.Handle("/", router)
@@ -125,77 +92,12 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	err := rootPageComponent.Render(r.Context(), w)
 	if (err != nil) {
-		log.Fatalf("Failed to render component: %v\n", err)
+		log.Printf("Failed to render component: %v\n", err)
 	}
 }
 
-func handleTable(w http.ResponseWriter, r *http.Request) {
-
-	tablePropsRowsAndData := generateTableData(1, 10, 3)
-
-	tableProps := components.TableProps{
-		Page: "1",
-		Headings: tablePropsHeadings,
-		RowsAndColumns: tablePropsRowsAndData,
-		Footers: tablePropsFooters,
-	}
-
-	tablePageProps := pages.TablePageProps{
-		TableProps: tableProps,
-	}
-
-	tablePageComponent := pages.TablePage(tablePageProps)
-
-	tablePageComponent.Render(r.Context(), w)
-}
-
-func handleDb(w http.ResponseWriter, r *http.Request) {
-	page := r.Header.Get("table-page")
-
-	pageInt, err := strconv.Atoi(page)
-	if (err != nil) {
-		log.Printf("Invalid table-page: [%v]. Using default page.\n", page)
-		pageInt = 0
-	}
-
-	pageInt += 1
-	page = strconv.Itoa(pageInt)
-	tableData := generateTableData(pageInt, 10, 3)
-
-	tableDataProps := components.TableDataProps{
-		Page: page,
-		RowsAndColumns: tableData,
-	}
-
-	tableDataComponent := components.TableData(tableDataProps)
-
-	tableDataComponent.Render(r.Context(), w)
-}
-
-func handleMultiReplace(w http.ResponseWriter, r *http.Request) {
-	page := pages.MultiReplacePage()
-	page.Render(r.Context(), w)
-}
-
-func generateTableData(page int, numRows int, numColumns int) [][]string {
-	result := [][]string{}
-
-	for row := 0; row < numRows; row++ {
-		newColumn := []string{}
-
-		for column := 0; column < numColumns; column++ {
-			newColumnData := fmt.Sprintf(
-				"row %d, column %d",
-				row + ((page - 1) * numRows) + 1,
-				column + 1,
-			)
-			newColumn = append(newColumn, newColumnData)
-		}
-
-		result = append(result, newColumn)
-	}
-
-	return result
+func handleHtmx(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>HTMX Page<h1>"))
 }
 
 func listenAndServe(router *mux.Router) {
